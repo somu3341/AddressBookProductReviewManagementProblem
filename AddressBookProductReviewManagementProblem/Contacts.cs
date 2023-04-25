a19a5678dedefcefe12cdabd40d159f2d42e1817
somu3341 <somashekarvsv1996@gmail.com>
2023-04-25 18:38:23 +0530

    [somu]Add:Adding New Contacts and Displaying.

--- AddressBookProductReviewManagementProblem/Contacts.cs
+++ AddressBookProductReviewManagementProblem/Contacts.cs
@@ -8,6 +8,7 @@ namespace AddressBookProductReviewManagementProblem
 {
     public class Contacts
     {
+        public int ContactId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
--- AddressBookProductReviewManagementProblem/Program.cs
+++ AddressBookProductReviewManagementProblem/Program.cs
@@ -19,14 +19,16 @@ namespace AddressBookProductReviewManagementProblem
                     case 1:
                         Console.WriteLine("Contact Details given in Table");
                         Console.WriteLine("------------------------------");
-                        contacts.Add(new Contacts { FirstName = "Soma", LastName = "Shekar", Address = "KGF", City = "Kolar", State = "KA", Zip = 563232, PhoneNumber = 9808789778, Email = "Somu@gmail.com" });
-                        contacts.Add(new Contacts { FirstName = "Santosh", LastName = "Kumar", Address = "Delhi", City = "Delhi", State = "DH", Zip = 545232, PhoneNumber = 8972789778, Email = "San@gmail.com" });
-                        contacts.Add(new Contacts { FirstName = "Anil", LastName = "Kumar", Address = "Banglore", City = "Banglore", State = "KA", Zip = 344432, PhoneNumber = 7808789778, Email = "Ani@gmail.com" });
-                        contacts.Add(new Contacts { FirstName = "Jagan", LastName = "Reddy", Address = "Hydrabad", City = "Hydrabad", State = "AP", Zip = 784732, PhoneNumber = 6808789778, Email = "Jagan@gmail.com" });
-                        contacts.Add(new Contacts { FirstName = "Ganesh", LastName = "Aryan", Address = "Manglore", City = "Manglore", State = "KA", Zip = 454332, PhoneNumber = 808789778, Email = "Ganesh@gmail.com" });
+                        contacts.Add(new Contacts { ContactId=1, FirstName = "Soma", LastName = "Shekar", Address = "KGF", City = "Kolar", State = "KA", Zip = 563232, PhoneNumber = 9808789778, Email = "Somu@gmail.com" });
+                        contacts.Add(new Contacts { ContactId = 2, FirstName = "Santosh", LastName = "Kumar", Address = "Delhi", City = "Delhi", State = "DH", Zip = 545232, PhoneNumber = 8972789778, Email = "San@gmail.com" });
+                        contacts.Add(new Contacts { ContactId = 3, FirstName = "Anil", LastName = "Kumar", Address = "Banglore", City = "Banglore", State = "KA", Zip = 344432, PhoneNumber = 7808789778, Email = "Ani@gmail.com" });
+                        contacts.Add(new Contacts { ContactId = 4, FirstName = "Jagan", LastName = "Reddy", Address = "Hydrabad", City = "Hydrabad", State = "AP", Zip = 784732, PhoneNumber = 6808789778, Email = "Jagan@gmail.com" });
+                        contacts.Add(new Contacts { ContactId = 5, FirstName = "Ganesh", LastName = "Aryan", Address = "Manglore", City = "Manglore", State = "KA", Zip = 454332, PhoneNumber = 808789778, Email = "Ganesh@gmail.com" });
+                        contacts.Add(new Contacts { ContactId = 5, FirstName = "Rajesh", LastName = "Kumar", Address = "Shivamoga", City = "Shivamoga", State = "KA", Zip = 564332, PhoneNumber = 888789778, Email = "Ganesh@gmail.com" });
+                        contacts.Add(new Contacts { ContactId = 5, FirstName = "Royal", LastName = "Challanger", Address = "Banglore", City = "Banglore", State = "KA", Zip = 564332, PhoneNumber = 708789778, Email = "Ganesh@gmail.com" });
                         foreach (var data in contacts)
                         {
-                            Console.WriteLine("FirstName:-"+ data.FirstName + " " + "LastName:-" + data.LastName + " " + "Address:-" + data.Address + " " + "City:-" + data.City + " " + "State" + data.State + " " + "Zip:-" + data.Zip + " " + "PhoneNumber:-" + data.PhoneNumber + " " + "Email:-" + data.Email);
+                            Console.WriteLine("ContactId:-"+data.ContactId+" "+ "FirstName:-"+ data.FirstName + " " + "LastName:-" + data.LastName + " " + "Address:-" + data.Address + " " + "City:-" + data.City + " " + "State" + data.State + " " + "Zip:-" + data.Zip + " " + "PhoneNumber:-" + data.PhoneNumber + " " + "Email:-" + data.Email);
                         }
                         break;
                     case 2:
